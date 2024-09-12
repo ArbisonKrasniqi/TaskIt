02b3f0b0ec1f3a9cde6561975b5dcbe0f824889c
endritmusaj <em67512@ubt-uni.net>
2024-09-12 17:38:32 +0200

    List title
    User is now able to edit the List title when it is pressed on, it also shows the Save and Cancel button.
    
    Also shows the error if the title being edited doest fulfill its criteria
    
    *Fixed the "No Boards Created" when all the boards were starred, it got changed when we merged the branches

--- frontend/src/Components/List/List.jsx
+++ frontend/src/Components/List/List.jsx
@@ -8,16 +8,52 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import { BoardContext } from "../BoardContent/Board.jsx";
 import ListDropdown from "../Dropdowns/ListDropdown.jsx";
+import { putData } from "../../Services/FetchService.jsx";
 
 const List = ({ list, children }) => {
   const boardContext = useContext(BoardContext);
-
+  const [isEditingListTitle, setIsEditingListTitle] = useState(false);
+  const [newListTitle, setNewListTitle] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const {getLists} = useContext(BoardContext);
   const handleShowTaskForm = (listId) => {
     boardContext.setSelectedListId(listId);
   }
 
   const handleHideTaskForm = () => {
     boardContext.setSelectedListId(null);
+  }
+
+  const handleListTitleChange = (e) => {
+    setNewListTitle(e.target.value)
+  }
+
+  const handleSaveListTitle = async () => {
+    try {
+      if (newListTitle.trim()) {
+        if (newListTitle.length < 2 || newListTitle.length > 20) {
+          setErrorMessage("List title must be between 2 and 20 characters.");
+          return;
+        }
+
+        const data = {
+          listId: list.listId,
+          title: newListTitle
+        };
+
+        await putData('http://localhost:5157/backend/list/UpdateList',data);
+        setIsEditingListTitle(false);
+        getLists();
+        setErrorMessage('');
+      }
+    } catch (error) {
+      
+    }
+  }
+
+  const handleCancelEdit = () => {
+    setNewListTitle('');
+    setIsEditingListTitle(false);
   }
 
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
@@ -41,11 +77,48 @@ const List = ({ list, children }) => {
       className={`flex-shrink-0 bg-gray-800 p-2.5 rounded-lg shadow-lg w-[275px] h-auto ${isDragging ? 'opacity-0' : ''}`}
     >
        <header className="flex justify-between items-center">
-              <h3 className="text-xl font-bold pb-4 text-gray-100 w-full h-full">{list.title}</h3>
-              <ListDropdown
+
+          {isEditingListTitle ? (
+                  <div className="flex flex-col justify-start space-x-2">
+                      <input
+                          type="text"
+                          value={newListTitle}
+                          onChange={handleListTitleChange}
+                          className="border rounded p-1 bg-gray-800 text-white font-bold"
+                          autoFocus
+                      />
+                      {errorMessage && (
+                        <p className="text-red-500">{errorMessage}</p>
+                      )}
+                      <div className="flex flex-row justify-start my-1">
+                      <button
+                          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded px-3 py-1"
+                          onClick={handleSaveListTitle}
+                      >
+                          Save
+                      </button>
+                      <button
+                          className="bg-gray-800 hover:bg-slate-700 text-gray-400 font-semibold rounded px-3 py-1"
+                          onClick={() => {handleCancelEdit(); setErrorMessage('')}}
+                      >
+                          Cancel
+                      </button>
+                      </div>
+                  </div>
+              ) : (
+                <h3 className="text-xl font-bold pb-4 text-gray-100 w-full h-full"
+                    onClick={() => {setIsEditingListTitle(true); setNewListTitle(list.title);}}>
+                      {list.title}
+                </h3>
+              )}
+
+
+              {isEditingListTitle ? ('') : (
+                <ListDropdown
                 listId={list.listId}
                 onAddCardClick={() => handleShowTaskForm(list.listId)}
               />
+              )}
         </header>
         {children}
         {boardContext.selectedListId === list.listId && (
