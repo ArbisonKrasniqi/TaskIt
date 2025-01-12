a74f1c32e4900cda3bf5795f359f252f77d49ead
ArbisonKrasniqi <ak64951@ubt-uni.net>
2025-01-12 01:39:49 +0100

    Update TaskDeleteHandler.cs
    Uncommented functions in taskdeletehandler

--- onion/backend/Application/Handlers/Task/TaskDeleteHandler.cs
+++ onion/backend/Application/Handlers/Task/TaskDeleteHandler.cs
@@ -1,3 +1,4 @@
+using Application.Handlers.Comment;
 using Domain.Entities;
 using Domain.Interfaces;
 
@@ -8,13 +9,14 @@ public class TaskDeleteHandler : ITaskDeleteHandler
     private readonly UserContext _userContext;
     private readonly ITasksRepository _tasksRepository;
     private readonly IWorkspaceActivityRepository _workspaceActivityRepository;
+    private readonly ICommentDeleteHandler _commentDeleteHandler;
     
-    public TaskDeleteHandler(UserContext userContext, ITasksRepository tasksRepository, IWorkspaceActivityRepository workspaceActivityRepository/*, ICommentDeleteHandler commentDeleteHandler*/)
+    public TaskDeleteHandler(UserContext userContext, ITasksRepository tasksRepository, IWorkspaceActivityRepository workspaceActivityRepository, ICommentDeleteHandler commentDeleteHandler)
     {
         _userContext = userContext;
         _tasksRepository = tasksRepository;
         _workspaceActivityRepository = workspaceActivityRepository;
-        //_commentDeleteHandler = commentDeleteHandler;
+        _commentDeleteHandler = commentDeleteHandler;
     }
     
     public async Task HandleDeleteRequest(int taskId)
@@ -23,8 +25,6 @@ public class TaskDeleteHandler : ITaskDeleteHandler
         var taskComments = task.Comments;
         if (taskComments.Any())
         {
-            //Nese ka komente ne task, 
-            //krijo nje for loop qe iteron ne te gjitha komentet
             foreach (var comment in taskComments)
             {
                 await _commentDeleteHandler.HandleDeleteRequest(comment.CommentId);
@@ -33,11 +33,11 @@ public class TaskDeleteHandler : ITaskDeleteHandler
         }
         await _tasksRepository.DeleteTask(taskId);
         
-        // var newActivity = new Domain.Entities.WorkspaceActivity(task.List.Board.Workspace.WorkspaceId,
-        //     _userContext.Id,
-        //     "Updated",
-        //     task.Title,
-        //     DateTime.Now);
-        // await _workspaceActivityRepository.CreateWorkspaceActivity(newActivity);
+        var newActivity = new Domain.Entities.WorkspaceActivity(task.List.Board.Workspace.WorkspaceId,
+            _userContext.Id,
+            "Updated",
+            task.Title,
+            DateTime.Now);
+        await _workspaceActivityRepository.CreateWorkspaceActivity(newActivity);
     }
 }
